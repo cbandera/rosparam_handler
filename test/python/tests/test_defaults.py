37c88d90294b59656cd1f9c8ee16046733bd2570
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2017-08-30 18:03:10 +0200

    Set default values on param server, add python tests for that

--- test/python/tests/test_defaults.py
+++ test/python/tests/test_defaults.py
@@ -1,5 +1,7 @@
 import unittest
 from rosparam_handler.param.DefaultsParameters import DefaultsParameters
+import rospy
+
 
 class TestDefaults(unittest.TestCase):
     def test_defaults(self):
@@ -16,3 +18,17 @@ class TestDefaults(unittest.TestCase):
         self.assertEqual(params.map_param_w_default, {"Hello": "World"})
         self.assertEqual(params.enum_param_w_default, 1)
 
+    def test_set_defaults_on_param_server(self):
+        params = DefaultsParameters()
+        # now all parameters should be set on param server
+        self.assertEqual(params.int_param_w_default, rospy.get_param("~int_param_w_default"))
+        self.assertAlmostEqual(params.double_param_w_default, rospy.get_param("~double_param_w_default"))
+        self.assertEqual(params.str_param_w_default, rospy.get_param("~str_param_w_default"))
+        self.assertEqual(params.bool_param_w_default, rospy.get_param("~bool_param_w_default"))
+
+        self.assertEqual(params.vector_int_param_w_default, rospy.get_param("~vector_int_param_w_default"))
+        self.assertEqual(params.vector_double_param_w_default, rospy.get_param("~vector_double_param_w_default"))
+        self.assertEqual(params.vector_string_param_w_default, rospy.get_param("~vector_string_param_w_default"))
+
+        self.assertEqual(params.map_param_w_default, rospy.get_param("~map_param_w_default"))
+        self.assertEqual(params.enum_param_w_default, rospy.get_param("~enum_param_w_default"))
