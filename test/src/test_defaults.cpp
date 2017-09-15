79c715f7c59060511fd4f2af838fd059aab65ea8
Claudio Bandera <cbandera@posteo.de>
2017-09-15 23:59:43 +0200

    ClangFormat files

--- test/src/test_defaults.cpp
+++ test/src/test_defaults.cpp
@@ -1,5 +1,5 @@
-#include <ros/ros.h>
 #include <gtest/gtest.h>
+#include <ros/ros.h>
 #include <rosparam_handler/DefaultsParameters.h>
 
 typedef rosparam_handler::DefaultsParameters ParamType;
@@ -14,12 +14,12 @@ TEST(RosparamHandler, Defaults) {
     ASSERT_EQ("Hello World", testParams.str_param_w_default);
     ASSERT_EQ(true, testParams.bool_param_w_default);
 
-    ASSERT_EQ(std::vector<int>({1,2,3}), testParams.vector_int_param_w_default);
+    ASSERT_EQ(std::vector<int>({1, 2, 3}), testParams.vector_int_param_w_default);
     ASSERT_EQ(std::vector<double>({1.1, 1.2, 1.3}), testParams.vector_double_param_w_default);
     ASSERT_EQ(std::vector<bool>({false, true}), testParams.vector_bool_param_w_default);
     ASSERT_EQ(std::vector<std::string>({"Hello", "World"}), testParams.vector_string_param_w_default);
 
-    std::map<std::string,std::string> tmp{{"Hello","World"}};
+    std::map<std::string, std::string> tmp{{"Hello", "World"}};
     ASSERT_EQ(tmp, testParams.map_param_w_default);
 
     ASSERT_EQ(1, testParams.enum_param_w_default);
@@ -83,23 +83,22 @@ TEST(RosparamHandler, DefaultsOnParamServer) {
     }
 }
 
-
 TEST(RosparamHandler, SetParamOnServer) {
     ros::NodeHandle nh("~");
     ParamType testParams(nh);
     ASSERT_NO_THROW(testParams.fromParamServer());
 
     testParams.int_param_w_default = 2;
-    testParams.double_param_w_default = 2.2; 
+    testParams.double_param_w_default = 2.2;
     testParams.str_param_w_default = "World Hello";
     testParams.bool_param_w_default = false;
-    testParams.vector_int_param_w_default = std::vector<int>{3,2,1};
-    testParams.vector_double_param_w_default = std::vector<double>{1.3,1.2,1.2};
+    testParams.vector_int_param_w_default = std::vector<int>{3, 2, 1};
+    testParams.vector_double_param_w_default = std::vector<double>{1.3, 1.2, 1.2};
     testParams.vector_bool_param_w_default = std::vector<bool>{true, false};
-    testParams.vector_string_param_w_default = std::vector<std::string>{"World","Hello"};
-    testParams.map_param_w_default = std::map<std::string,std::string>{{"World","Hello"}};
-    testParams.enum_param_w_default =  2;
-    
+    testParams.vector_string_param_w_default = std::vector<std::string>{"World", "Hello"};
+    testParams.map_param_w_default = std::map<std::string, std::string>{{"World", "Hello"}};
+    testParams.enum_param_w_default = 2;
+
     testParams.toParamServer();
 
     // values should now be set on param server
